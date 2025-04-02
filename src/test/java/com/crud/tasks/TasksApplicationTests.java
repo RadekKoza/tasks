ec084a7eef27f0b2bb9468a4a387a64711d4606d
RadekKoza <radekk1@poczta.fm>
2025-04-02 19:18:01 +0200

    Zad.17.3 stworzenie repozytorium i przesłanie kodu

---
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -0,0 +1,21 @@
+package com.crud.tasks;
+
+import com.crud.tasks.domain.TaskDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void testDto() {
+		//Given
+		//When
+
+		//Then
+
+	}
+
+}
